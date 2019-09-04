8c18281582485cbe471da354483e69bac5c79b8e
Lei Gao <32151560+GaoLei-bio@users.noreply.github.com>
2019-09-04 16:56:33 -0400

    Create Covert_to_Int.py
    convert field elements to int

---
+++ Covert_to_Int.py
@@ -0,0 +1,6 @@
+def convert_int(fields):
+    for i in range(0,len(fields)):
+        if fields[i].isdigit() or (fields[i].startswith("-") and fields[i][1:].isdigit()):
+            fields[i] = int(fields[i])
+    return fields
+    
